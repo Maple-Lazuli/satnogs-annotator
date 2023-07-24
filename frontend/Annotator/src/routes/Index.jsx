40b98846f7929627306af2cec68f726845983722
Ada <the.nostra.tymus@gmail.com>
2023-07-24 04:04:23 -0400

    updated the details on the index

--- frontend/Annotator/src/routes/Index.jsx
+++ frontend/Annotator/src/routes/Index.jsx
@@ -2,12 +2,12 @@ export default function Index() {
 
     return(
         <div className="jumbotron">
-        <h1 className="display-4">Inventory Manager</h1>
-        <p className="lead">This is a simple Create Read Update Delete (CRUD) App for managing inventory.</p>
+        <h1 className="display-4">SATNOGS Annotator</h1>
+        <p className="lead">This is a tool for annotating SATNOGS Spectrograms</p>
         <hr className="my-4"/>
-        <p>Use the button below to see the current inventory</p>
+        <p>Use the button below to see the current annotations</p>
         <p className="lead">
-            <a className="btn btn-outline-primary btn-lg" href="/AllItems" role="button">View Current Inventory</a>
+            <a className="btn btn-outline-primary btn-lg" href="/AllItems" role="button">View Current Annotations</a>
         </p>
         </div>
     )
